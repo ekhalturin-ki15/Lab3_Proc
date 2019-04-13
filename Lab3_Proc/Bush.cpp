ff03392d2a5b980c1af60d958056a696dd8eff93
1 <1>
2019-04-13 23:47:58 +0700

    Рабочая версия

--- Lab3_Proc/Bush.cpp
+++ Lab3_Proc/Bush.cpp
@@ -3,7 +3,7 @@
 void InBush(std::ifstream& infile, Bush& b)
 {
 	std::string s;
-	infile >> b.month >> s;
+	infile >> b.month >> s >> b.WIG;
 	if (s.length() < 20)
 		strcpy_s(b.name, s.c_str());
 }
@@ -15,7 +15,7 @@ void OutBush(std::ofstream& outfile, Bush b)
 
 	outfile << "Это кустарник, ";
 
-	if (1 <= b.month && b.month <= watIsMonth.size())
+	if ((1 <= b.month) && (b.month <= watIsMonth.size()))
 	{
 		outfile << "месяц цветения - " << watIsMonth[b.month - 1];
 
@@ -25,6 +25,15 @@ void OutBush(std::ofstream& outfile, Bush b)
 		outfile << "месяц цветения считался некорректно";
 	}
 
-	outfile << ", Его название: " << b.name << "\n";
+	outfile << ", Его название: " << b.name << " ";
+	if ((1 <= b.WIG) && (b.WIG <= BwhereItGrows.size()))
+	{
+		outfile << "; Растёт в " << BwhereItGrows[b.WIG - 1];
+	}
+	else
+	{
+		outfile << "; Местность введенна некорректно";
+	}
+	outfile << std::endl;
 
 }
